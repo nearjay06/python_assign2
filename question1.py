0bd322bdc3d185a1416e7e67af8f66dd20654f3f
nearjay06 <nearjoan2018@gmail.com>
2019-08-20 14:33:39 +0300

    python assignment 2

---
+++ question1.py
@@ -0,0 +1,7 @@
+def list_numbers():
+    items =[1,3,5,6,9,6,8]
+    for numbers in items:
+        if numbers > 2 and numbers < 9:
+            print(numbers)
+
+list_numbers()
---
+++ question2.py
@@ -0,0 +1,10 @@
+def even_numbers():
+    sample = [1,2,3,4,5,6,7,8,9]
+    for i in sample:
+        if i%2 == 0:
+            print (i)
+        else:
+            print("not an even number")
+
+
+even_numbers()
